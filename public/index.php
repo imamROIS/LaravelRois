0c6143229441b942c2e4d04fe8600078d8b05254
imamROIS <roisbudiman88@gmail.com>
2025-03-15 03:41:48 +0700

    mengganti file env

--- public/index.php
+++ public/index.php
@@ -11,16 +11,16 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 }
 
 
-// require __DIR__.'/bootstrap/autoload.php';
-// $app = require_once __DIR__.'/bootstrap/app.php';
+require __DIR__.'/bootstrap/autoload.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
 
 
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+// // Register the Composer autoloader...
+// require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+// // Bootstrap Laravel and handle the request...
+// /** @var Application $app */
+// $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
